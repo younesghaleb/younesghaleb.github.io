0478086cb55a53c227651710d4302be183355546
younesghaleb <77145546+younesghaleb@users.noreply.github.com>
2021-05-13 10:40:21 +0200

    Update leerFicheroXML.js

--- leerFicheroXML.js
+++ leerFicheroXML.js
@@ -1,8 +1,8 @@
 function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
-	let alumnos = xmlDoc.querySelectorAll("alumno")
-	for(let i=0; i<alumnos.length; i++)
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + alumnos[i].textContent + "</p>"
+	let libros = xmlDoc.querySelectorAll("libro")
+	for(let i=0; i<libros.length; i++)
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
 	
 }
 
