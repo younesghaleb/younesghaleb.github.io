474680e144a93ff5a99f990b437b0bd0b3d75bd1
younesghaleb <77145546+younesghaleb@users.noreply.github.com>
2021-05-17 09:32:30 +0200

    Add files via upload

---
+++ ex1/leerFicheroXML.js
@@ -0,0 +1,14 @@
+function gestionarFicheroXML(xmlDoc){
+	let capaVacia = document.querySelector("#ficheroXML")
+	let libros = xmlDoc.querySelectorAll("libro")
+	for(let i=0; i<libros.length; i++)
+		if (i == 2 ) 
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='sub'>" + libros[i].textContent + "</p>"
+	        else
+					capaVacia.innerHTML = capaVacia.innerHTML + "<p class='borde'>" + libros[i].textContent + "</p>"
+
+	
+}
+
+
+	loadDocA("libros.xml","xml");
