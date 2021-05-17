38b499af025fc5bd62d550a5e9539fabc60bbf21
younesghaleb <77145546+younesghaleb@users.noreply.github.com>
2021-05-17 09:02:53 +0200

    Update leerFicheroXML.js

--- leerFicheroXML.js
+++ leerFicheroXML.js
@@ -2,7 +2,11 @@ function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
 	let libros = xmlDoc.querySelectorAll("libro")
 	for(let i=0; i<libros.length; i++)
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
+		if (i == 2 ) then 
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class'sub'>" + libros[i].textContent + "</p>"
+	        else
+					capaVacia.innerHTML = capaVacia.innerHTML + "<p class'borde'>" + libros[i].textContent + "</p>"
+
 	
 }
 
