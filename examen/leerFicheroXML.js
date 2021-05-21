d6d10b5fa3cacb07078ad29cd1f339ebd0ee326c
younesghaleb <77145546+younesghaleb@users.noreply.github.com>
2021-05-21 09:47:33 +0200

    Update leerFicheroXML.js

--- examen/leerFicheroXML.js
+++ examen/leerFicheroXML.js
@@ -2,12 +2,10 @@ function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#cinexml")
 	let cine = xmlDoc.querySelector("titulo")
 	
-	let cine2 = xmlDoc.querySelector("director")
-	let cine3 = xmlDoc.querySelectorAll("fecha")
 
 
 		
-	capaVacia.innerHTML = capaVacia.innerHTML + "<p class='name'>" + cine.textcontent + "</p>"
+	capaVacia.innerHTML = capaVacia.innerHTML + "<p class='name'>" + cine[i].textcontent + "</p>"
 	   
 
 	
