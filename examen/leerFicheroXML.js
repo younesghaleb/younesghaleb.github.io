af3c7987f3c9105442125413507398cde15d653f
younesghaleb <77145546+younesghaleb@users.noreply.github.com>
2021-05-21 09:39:34 +0200

    Add files via upload

---
+++ examen/leerFicheroXML.js
@@ -0,0 +1,18 @@
+function gestionarFicheroXML(xmlDoc){
+	let capaVacia = document.querySelector("#cinexml")
+	let cine = xmlDoc.querySelector("titulo")
+	
+	let cine2 = xmlDoc.querySelector("director")
+	let cine3 = xmlDoc.querySelectorAll("fecha")
+
+
+		
+	capaVacia.innerHTML = capaVacia.innerHTML + "<p class='name'>" + + "</p>"
+	   
+
+	
+}
+
+
+	loadDocA("cine.xml","xml");
+
